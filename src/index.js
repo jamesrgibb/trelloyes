89bfdce5d8c412d7a609ed61a83daa1dabdaac48
jamesrgibb <51032141+jamesrgibb@users.noreply.github.com>
2019-11-21 09:45:51 -0700

    edits

--- src/index.js
+++ src/index.js
@@ -10,3 +10,12 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+import './index.css';
+import STORE from './STORE'
+
+ReactDOM.render(
+  <App store={STORE} />,
+  document.getElementById('root'),
+);
